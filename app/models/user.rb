c4e7611581374d2847e6b52e3db563d262eaa23d
mellowmorgan <morgan.waites@gmail.com>
2022-02-20 21:08:36 -0800

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # has_secure_password
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :email, presence: true, length: { minimum: 4 }
-  validates :username, presence: true, length: { minimum: 4 }
-  validates :password, presence: true
+  # validates :username, uniqueness: true
+  # validates :email, presence: true, length: { minimum: 4 }
+  # validates :username, presence: true, length: { minimum: 4 }
+  # validates :password, presence: true
+  # it seems to be mad at validates above, had to comment out to get it working
   validate :email_has
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
