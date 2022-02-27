8e688d329612cab928e37964e6172738cd684b33
mellowmorgan <morgan.waites@gmail.com>
2022-02-26 23:39:21 -0800

    WIP: styling homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   include Devise::Controllers
 
   def index
-    @requests_published_10 = Request.most_recently_added_published
-    @requests_approved_10 = Request.most_recently_added_approved
+    @requests_published = Request.most_recently_added_published
+    @requests_approved = Request.most_recently_added_approved
     render :index
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,6 @@ class Request < ApplicationRecord
   validates :content, presence: true
   validates :status, presence: true, inclusion: { in: ["submitted","approved","published","flagged","denied"]}
   has_many :descriptions
-  scope :most_recently_added_published, -> { where(status:"published").order(created_at: :desc).limit(10)}
-  scope :most_recently_added_approved, -> { where(status:"approved").order(created_at: :desc).limit(10)}
+  scope :most_recently_added_published, -> { where(status:"published").order(created_at: :desc)}
+  scope :most_recently_added_approved, -> { where(status:"approved").order(created_at: :desc)}
 end
