f6a32419277e4652cb3ee3a0bc38dd21b38abe85
mellowmorgan <morgan.waites@gmail.com>
2022-03-01 17:29:13 -0800

    WIP: validate custom validation categories

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,7 @@ class Request < ApplicationRecord
   belongs_to :user
   before_save :downcase_categories
   validates :content, presence: true
+  validate :categories_must_exist
   validates :status, presence: true, inclusion: { in: ["submitted","approved","published","flagged","denied"]}
   has_many :descriptions
   scope :most_recently_added_published, -> { where(status:"published").order(created_at: :desc)}
@@ -9,4 +10,18 @@ class Request < ApplicationRecord
   def downcase_categories
     self.categories = self.categories.map{|word| word.downcase }
   end
+
+  def categories_must_exist
+    valid_categories=["people","popular","nature","miscellaneous","objects","landmarks","buildings","animals"]
+    valid_checker=true
+    self.categories.each do |word|
+      if !valid_categories.include?(word.downcase)
+        valid_checker=false
+      end
+    end
+
+  if categories.any? && !valid_checker
+    errors.add(:categories, "You've entered an invalid category. Please include one or more of the following categories: people, popular, nature, miscellaneous, objects, landmarks, buildings, animals")
+  end
 end
+
