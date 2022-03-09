29eee95e1a674971db9286f3379fbe4034188af9
mellowmorgan <morgan.waites@gmail.com>
2022-03-08 16:53:13 -0800

    get rid of env

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
   config.cloud_name = 'dntx9ehds'
   config.api_key = '646947548162925'
-  config.api_secret = ENV['API_SECRET_CLOUDINARY']
+  config.api_secret = 'Ycp2wBI7hxz2vhj4ls9GSSe2OPU'
   config.secure = true
   config.cdn_subdomain = true
 end
